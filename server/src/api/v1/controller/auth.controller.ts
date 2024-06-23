ac66237f249b71be240fe4d76e788629bbd772a6
rishadpt <rishadpt16@gmail.com>
2024-06-23 10:07:33 +0530

    feat: project initialised

---
+++ server/src/api/v1/controller/auth.controller.ts
@@ -0,0 +1,12 @@
+import authService from "../services/auth.service";
+
+class authController {
+
+    loginController = (req: Request, res: Response)=>{
+
+        const body = req.body
+        authService.loginService(body)
+    }
+}
+
+export default new authController();
