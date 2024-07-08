3ce7a3dea7d5f78487346ddcac1d484a8f3cfb1e
manshad <manshadchangampalli010@gmail.com>
2024-07-08 23:47:13 +0530

    the trial balance completed

--- client/src/pages/reports/trailBalance/TrailBalance.tsx
+++ client/src/pages/reports/trailBalance/TrailBalance.tsx
@@ -7,6 +7,10 @@ import TrialTable from './components/trial-table'
 import { useEffect, useState } from 'react'
 import moment from 'moment'
 import DateFilter from '../../../components/filterComponent/dateFilter/DateFilter'
+import { IFormatedData, ITrialBalanceData } from '../../../types/trialBalance/trialBalance'
+
+
+
 
 const breadCrumbData = [
     { name: 'Dashboard', link: '/' },
@@ -17,6 +21,7 @@ const TrailBalance = () => {
     const [, setSearchParams] = useSearchParams();
     const [fromDate, setFromDate] = useState(moment('2024-04-01').format('YYYY-MM-DD'));
     const [toDate, setToDate] = useState(moment().format('YYYY-MM-DD'));
+    const [formatedData, setFormatedData] = useState<IFormatedData | null>(null)
     const handleDateFromChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const newFromDate = moment(event.target.value).format('YYYY-MM-DD');
         setFromDate(newFromDate);
@@ -35,6 +40,45 @@ const TrailBalance = () => {
     }, [fromDate, toDate, setSearchParams]);
     const url = `reports/trial-balance?fromDate=${fromDate}&toDate=${toDate}`
     const { data, isPending } = useQueryGetApi(url)
+    const getFormatData = (data: ITrialBalanceData[]) => {
+        console.log(data)
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const store: any = {
+            asset: { children: [] },
+            liability: { children: [] },
+            Purchase: { children: [] },
+            Sales: { children: [] }
+        }
+        for (const item of data) {
+            if (item?.ledger === 'Purchase') {
+                store.Purchase = item;
+                continue;
+            }
+            if (item?.ledger === 'Sales') {
+                store.Sales = item;
+                continue;
+            }
+            if (item?.ledger === 'Total') {
+                if (store?.[item?.type]) {
+                    const obj = store[item?.type]
+                    obj && (obj['total_credit'] = item?.total_credit)
+                    obj && (obj['total_debit'] = item?.total_debit)
+                }
+                continue;
+            }
+            if (store?.[item?.type]) {
+                const obj = store[item?.type]
+                obj?.children && obj.children.push(item)
+                continue;
+            }
+        }
+        setFormatedData(store)
+    }
+    useEffect(() => {
+        if (data?.data) {
+            getFormatData(data?.data)
+        }
+    }, [data])
     return (
         <>
             {
@@ -58,7 +102,7 @@ const TrailBalance = () => {
                     />
                 </div>
                 <section className='pt-5 pb-2'>
-                    <TrialTable data={data} />
+                    <TrialTable data={formatedData} />
                 </section>
             </main>
         </>
