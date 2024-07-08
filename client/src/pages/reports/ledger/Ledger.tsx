9e7a41ff7b0b0dc141b37bac6b824be218aba89c
manshad <manshadchangampalli010@gmail.com>
2024-07-08 12:28:13 +0530

    fix: the ledger empty message added

--- client/src/pages/reports/ledger/Ledger.tsx
+++ client/src/pages/reports/ledger/Ledger.tsx
@@ -126,7 +126,11 @@ const Ledger = () => {
                     />
                 </div>
                 <section className="pt-5 pb-2">
-                    <Table balanceFooterComponent={<FooterResult />} data={tableData} showFooterBalance columnData={ColumnData} hideFooter />
+                    {searchParams.get('name') ? <Table balanceFooterComponent={<FooterResult />} data={tableData} showFooterBalance columnData={ColumnData} hideFooter /> :
+                        <div className="grid place-items-center">
+                            <h2>Select Ledger</h2>
+                        </div>
+                    }
                 </section>
             </main>
         </>
